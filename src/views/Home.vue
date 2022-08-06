9be9be40999bab56d3ec56ce2728578bdd681d55
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-06 16:58:03 +0430

    add slider to project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at quisquam animi ad optio explicabo earum natus id tempore. Deserunt ab, tempora, aliquam iusto ipsam eum ut consequuntur quod vero sint quisquam amet doloremque unde totam nobis sed praesentium, sunt harum. Officiis dignissimos corrupti ipsa suscipit eum reiciendis quaerat? Blanditiis magni saepe recusandae ex ipsum obcaecati fuga facere, repellat optio! Libero cumque, ipsum laborum alias quos nisi nam amet aspernatur, ad deleniti rem error perspiciatis sapiente odio, obcaecati esse? Consequatur quis est labore facere mollitia sequi, sed eos velit. Incidunt, autem voluptate reprehenderit eius aperiam quas laudantium soluta iusto eligendi.</p>
+       
+    <Carousel/>
     </div>
 </template>
 
 <script>
+import Carousel from '@/components/Carousel.vue';
     export default {
-        name:'HomeC'
-    }
+    name: "HomeC",
+    components: { Carousel }
+}
 </script>
 
 <style lang="scss" scoped>
