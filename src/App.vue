8a147db6ae44245c7964c4cedb959d8bbf8c21b6
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-06 15:45:14 +0430

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
- 
-  <router-view/>
+ <div class="app">
+  <Header/>
+   <router-view/>
+ </div>
 </template>
+<script>
+import Header from './components/Header.vue';
 
+  export default{
+    components: { Header }
+}
+</script>
 <style>
 @font-face {
   font-family: "Yekan";
   src: local("Yekan"),
    url(./assets/B_Yekan/Yekan.ttf) format("truetype");
 }
+
+.app{
+  font-family: Yekan;
+  direction: rtl;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+            <header>
+                <nav>
+                    <ul>
+                        <li>خانه</li>
+                        <li>قوانین</li>
+                        <li>درباره ما</li>
+                        <li>محصولات</li>
+                    </ul>
+                    <ul>
+                        <li>ورود/ثبت نام</li>
+                    </ul>
+                </nav>
+            </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HeaderComponent'
+    }
+</script>
+
+<style  scoped>
+nav{
+    display: flex;
+    justify-content: space-between;
+}
+li{
+    list-style: none;
+}
+</style>
