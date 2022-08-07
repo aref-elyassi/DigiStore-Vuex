ff366a811661c021a089ee9cc67510b473c31678
aref_elyassi <aref_elyassi1992@yahoo.com>
2022-08-07 11:02:51 +0430

    add data to product.js

---
+++ src/store/product.js
@@ -0,0 +1,73 @@
+export const Products=[
+    {
+        id:1,
+        name:'Samsung Galaxy S22',
+        price:40000000,
+        image:'',
+        description:'',
+        pr_id:1
+    },
+    {
+        id:2,
+        name:'Samsung Galaxy S21',
+        price:20000000,
+        image:'',
+        description:'',
+        pr_id:1
+    },
+    {
+        id:3,
+        name:'ساعت هوشمند شیائومی',
+        price:1000000,
+        pr_id:2
+    },
+    {
+        id:4,
+        name:' یخچال ساید بای ساید ال جی',
+        price:100000000,
+        pr_id:3
+    },
+    {
+        id:5,
+        name:'یخچال ساید بای ساید سامسونگ',
+        price:120000000,
+        pr_id:3
+    },
+    {
+        id:6,
+        name:'تلویزیون ال ای دی سامسونگ',
+        price:50000000,
+        pr_id:4
+    },
+    {
+        id:7,
+        name:'تلویزیون ال جی',
+        price:60000000,
+        pr_id:4
+    },
+    {
+        id:8,
+        name:'لباسشویی ال جی',
+        price:15000000,
+        pr_id:5
+    },
+    {
+        id:9,
+        name:'لباسشویی سامسونگ',
+        price:20000000,
+        pr_id:5
+    },
+    {
+        id:10,
+        name:'ساعت هوشمند سامسونگ',
+        price:10000000,
+        pr_id:2
+    },
+    {
+        id:11,
+        name:'Samsung Galaxy A52S',
+        price:12000000,
+        pr_id:1
+    },
+
+]
